824298e931c592d39556bf40da5c89bb805345f0
Hjordan023 <higor.jundiai@gmail.com>
2025-07-18 12:07:09 -0300

    criação do html css e js

---
+++ Math.js
@@ -0,0 +1,14 @@
+
+
+
+/*
+-pow(2,2) potencia /2² = 4
+-sqrt(25,2) raiz quadrada
+-ceil/teto > arrendoda para cima 
+-floor/chão > arredonda para baixo
+-random = ele gera um numero aleatorio entre 0 e 1
+OBS: Sempre separar os numeros por ponto e nao virgula 
+
+*/
+const result = Math.random()
+console.log(result)
